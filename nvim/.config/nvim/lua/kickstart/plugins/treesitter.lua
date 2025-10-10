d0e6c2eea0010c5d4d7fe1c2346e5b6e05c866ba
l86 <alexisaglar@gmail.com>
2025-10-10 17:40:53 +0100

    treesitter changes

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,28 +1,19 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    -- Use `opts` instead of `config`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
+      ensure_installed = { 'lua', 'python', 'vim', 'vimdoc' }, -- Add any other languages you use
+      sync_install = false, -- Install parsers synchronously (blocks UI)
+      auto_install = true, -- Automatically install parsers when entering a buffer
       highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        enable = true, -- Enable syntax highlighting
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = {
+        enable = true, -- Enable indentation based on treesitter
+      },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
